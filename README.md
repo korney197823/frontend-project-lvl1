c678695028eec2ff9e43ea66d9a2f2f855e0261e
Denis Kornienko <korney197823@gmail.com>
2020-02-05 23:15:33 +0300

    add eslint

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Maintainability](https://api.codeclimate.com/v1/badges/866e256647efaa51bca1/maintainability)](https://codeclimate.com/github/korney197823/frontend-project-lvl1/maintainability)
+![](https://github.com/korney197823/frontend-project-lvl1/.github/workflows/integration.yml/badge.svg)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
 const welcome = (gameName = '\n') => {
-    console.log('Welcome to the Brain Games!');
-    console.log(gameName);
-    const name = readlineSync.question('May i have your name?: ');
-    console.log(`Hello, ${name}`);
+  console.log('Welcome to the Brain Games!');
+  console.log(gameName);
+  const name = readlineSync.question('May i have your name?: ');
+  console.log(`Hello, ${name}`);
 };
 
-export { welcome };
+export default welcome;
