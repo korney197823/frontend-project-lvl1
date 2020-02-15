db54869d5ddfe361f7624e767e569a589b2d439a
Denis Kornienko <korney197823@gmail.com>
2020-02-15 14:12:35 +0300

    add brain-even games

---
+++ bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { welcome, brainEven } from '../src/index.js';
+
+welcome.title();
+welcome.hello();
+brainEven();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
 import readlineSync from 'readline-sync';
 
-const welcome = (gameName = '\n') => {
-  console.log('Welcome to the Brain Games!');
-  console.log(gameName);
-  const name = readlineSync.question('May i have your name?: ');
-  console.log(`Hello, ${name}`);
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100);
+}
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+const welcome = {
+  title() {
+    console.log('Welcome to the Brain Games!');
+  },
+  userName: readlineSync.question('May i have your name?: '),
+  hello() {
+    console.log(`Hello, ${this.userName}`);
+  },
+  getName() {
+    return this.userName;
+  },
 };
 
-export default welcome;
+const brainEven = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let countCorrectAnswers = 0;
+
+  while (countCorrectAnswers < 3) {
+    const number = getRandomNumber();
+
+    console.log('Question: ', number);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    if ((isEven(number) && answer === 'yes') || (!isEven(number) && answer === 'no')) {
+      console.log('Correct!');
+      countCorrectAnswers += 1;
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${answer === 'yes' ? 'no' : 'yes'}`);
+      break;
+    }
+  }
+  if (countCorrectAnswers === 3) {
+    console.log(`Congratulations, ${welcome.getName()}!`);
+  }
+};
+
+export { welcome, brainEven };
