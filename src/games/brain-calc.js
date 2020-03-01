c99c6777ef0eaa3ff7f7021cf18be97eb7616793
Denis Kornienko <korney197823@gmail.com>
2020-03-01 14:05:05 +0300

    Refactored brain calc games, changet if/else expression to switch/case

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -11,20 +11,28 @@ const calc = () => {
   const operation = getRandomOperation();
   const numberOne = getRandomNumber(100);
   const numberTwo = getRandomNumber(100);
+  const question = `${numberOne} ${operation} ${numberTwo}`;
 
-  let question = '';
-  let correctAnswer = 0;
-
-  if (operation === '+') {
-    question = `${numberOne} + ${numberTwo}`;
-    correctAnswer = numberOne + numberTwo;
-  } else if (operation === '*') {
-    question = `${numberOne} * ${numberTwo}`;
-    correctAnswer = numberOne * numberTwo;
-  } else if (operation === '-') {
-    question = `${numberOne} - ${numberTwo}`;
-    correctAnswer = numberOne - numberTwo;
+  function computedResult(numOne, numTwo, operator) {
+    let result = 0;
+    switch (operator) {
+      case '+':
+        result = numOne + numTwo;
+        break;
+      case '-':
+        result = numOne - numTwo;
+        break;
+      case '*':
+        result = numOne * numTwo;
+        break;
+      default:
+        return result;
+    }
+    return result;
   }
+
+  const correctAnswer = computedResult(numberOne, numberTwo, operation);
+
   console.log(description);
   console.log('Question: ', question);
   const answer = Number(readlineSync.question('Your answer: '));
